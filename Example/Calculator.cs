29943667ed90c766f781a327ca1aa588faa20958
James Gray <jimgray2005@gmail.com>
2018-09-09 10:22:00 -0400

    added test steps to classes

--- Example/Calculator.cs
+++ Example/Calculator.cs
@@ -8,6 +8,12 @@ namespace Example
 {
     public class Calculator
     {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
 
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
     }
 }
--- SpecFlowTestProj/CalculatorSteps.cs
+++ SpecFlowTestProj/CalculatorSteps.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using TechTalk.SpecFlow;
 using Example;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SpecFlowTestProj
 {
@@ -8,29 +9,30 @@ namespace SpecFlowTestProj
     public class CalculatorSteps
     {
         private Calculator calculator = new Calculator();
+        private int result;
 
         [Given(@"I have entered (.*) into the calculator")]
-        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
         {
-            ScenarioContext.Current.Pending();
+            calculator.FirstNumber = number;
         }
         
         [Given(@"I have also entered (.*) into the calculator")]
-        public void GivenIHaveAlsoEnteredIntoTheCalculator(int p0)
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
         {
-            ScenarioContext.Current.Pending();
+            calculator.SecondNumber = number;
         }
         
         [When(@"I press add")]
         public void WhenIPressAdd()
         {
-            ScenarioContext.Current.Pending();
+            result = calculator.Add();
         }
         
         [Then(@"the result should be (.*) on the screen")]
-        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
         {
-            ScenarioContext.Current.Pending();
+            Assert.AreEqual(expectedResult, result);
         }
     }
 }
